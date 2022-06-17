8a3a63a149c2d94db45514745b7c879529558bc1
m02242a <Brian.Harford@experian.com>
2022-06-17 17:27:21 -0400

    variables, constants & comments

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,18 @@
-console.log(1);
-console.log(2);
-alert('hello, world');
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+points = 333; // gets an error - Uncaught TypeError: Assignment to constant variable.
+
+// used to use 'var' but var is old now (do not use)
+var score = 75;
+console.log(score);
