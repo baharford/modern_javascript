52500da82dd8ec40b130353ccfe59a9ad1bad845
Brian Harford <baharford@gmail.com>
2022-06-23 19:02:31 -0400

    Returning Values

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,8 @@
-// arguments & parameters
+// returning values
 
-const speak = function(name = 'luigi', time = 'night') { 
-    console.log(`good ${time} ${name} !`);
+const calcArea = function(radius) { 
+    return 3.14 * radius**2;
 }
 
-speak(); // function will use default values if we don't assign
-speak('mario', 'morning');
+const area = calcArea(5);
+console.log(area);
