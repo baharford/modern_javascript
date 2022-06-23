a7db902d86561cd3143ef1eae0836887dfde9561
Brian Harford <baharford@gmail.com>
2022-06-23 19:28:47 -0400

    Foreach Method & Callbacks

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,39 @@
-const name = 'shaun';
+// callbacks & foreach
 
-// functions - defined on its own
-const greet = () => 'hello';
-let resultOne = greet();
-console.log(resultOne);
+const myFunc = (callbackFunc) => { 
+    // do something
+    let value = 50;
+    callbackFunc(value);
+};
+
+// for argument we pass in function - this is a "callback"
+myFunc(function(value){
+    // do something
+    console.log(value);
+});
+
+// we can turn the callback into an arrow function as well
+myFunc(value => {
+    // do something
+    console.log(value);
+});
 
 
-// methods - are functions , but functions associated with a data type
-console.log(name.toUpperCase());
+// foreach to loop through an array with a callback
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
+// people.forEach(function(person) {
+//     console.log(person);
+// });
+
+// can convert this into arrow function too
+// people.forEach((person,index) => {
+//     console.log(index,person);
+// });
+
+// can use arrow function with callback as well 
+const logPerson = (person, index) => { 
+    console.log(`${index} - hello ${person}`);
+};
+
+people.forEach(logPerson);
