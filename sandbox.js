1f36e994f879abcce7c344390c512a144832deb6
Brian Harford <baharford@gmail.com>
2022-06-23 19:34:19 -0400

    Callback Functions in Action

--- sandbox.js
+++ sandbox.js
@@ -1,39 +1,14 @@
-// callbacks & foreach
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
 
-const myFunc = (callbackFunc) => { 
-    // do something
-    let value = 50;
-    callbackFunc(value);
-};
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
-// for argument we pass in function - this is a "callback"
-myFunc(function(value){
-    // do something
-    console.log(value);
+let html = ``;
+
+people.forEach(function(person) {
+    // create html template 
+    html += `<li style="color: purple">${person}</li>`;
 });
 
-// we can turn the callback into an arrow function as well
-myFunc(value => {
-    // do something
-    console.log(value);
-});
-
-
-// foreach to loop through an array with a callback
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
-
-// people.forEach(function(person) {
-//     console.log(person);
-// });
-
-// can convert this into arrow function too
-// people.forEach((person,index) => {
-//     console.log(index,person);
-// });
-
-// can use arrow function with callback as well 
-const logPerson = (person, index) => { 
-    console.log(`${index} - hello ${person}`);
-};
-
-people.forEach(logPerson);
+console.log(html);
+ul.innerHTML = html;
