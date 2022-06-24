c707d0f6b75b0ab2f8319a3f173cc4bff90493cc
Brian Harford <baharford@gmail.com>
2022-06-24 10:03:08 -0400

    Adding Methods to an Object

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,18 @@
-// creating an object literal
+// adding methods to an object
 
 let user = { 
     name: 'crystal',
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'Berlin',
-    blogs: ['why mac and cheese rules', '10 things to make with marmite']
+    blogs: ['why mac and cheese rules', '10 things to make with marmite'],
+    login: function() {
+        console.log('the user logged in');
+    },
+    logout: function() { 
+        console.log('user logged out');
+    }
 };
 
-// using dot notation
-console.log(user);
-console.log(user.name);
-
-// how to update property on an object
-user.age = 35;
-console.log(user.age);
-
-// can use square bracket notation
-console.log(user['name']);
-console.log(user['email']);
-user['name'] = 'chun-li';
-console.log(user['name']);
+user.login();
+user.logout();
