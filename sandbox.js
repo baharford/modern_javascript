f1abd73e28070fe731511ab084170d7ef56055d4
Brian Harford <baharford@gmail.com>
2022-06-24 10:12:39 -0400

    Objects in Arrays

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,26 @@
 // using 'this' keyword with an object
 
+const blogs = [
+    { title: 'why mac and cheese rules', likes: 30 },
+    { title: '10 things to make with marmite', likes: 50 },
+];
+
 let user = { 
     name: 'crystal',
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'Berlin',
-    blogs: ['why mac and cheese rules', '10 things to make with marmite'],
-    login: function() {
+    blogs: blogs,
+    login() {
         console.log('the user logged in');
     },
-    logout: function() { 
+    logout() { 
         console.log('user logged out');
     },
-    logBlogs: function() { 
+    logBlogs() { 
         console.log('this user has written the following blogs:');
         this.blogs.forEach(blog => {
-            console.log(blog);
+            console.log(`title: ${blog.title} , likes: ${blog.likes}`);
         });
     }
 };
