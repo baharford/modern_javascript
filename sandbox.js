64c1c640a8bc9016aa2771a76f978a9aee68bdb7
Brian Harford <baharford@gmail.com>
2022-06-26 15:09:03 -0400

    Creating & Removing Elements

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,20 @@
-// Event Basics (click events)
+// Creating and removing elements
 
+const ul = document.querySelector('ul');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => { 
-    console.log('you clicked me');
+    const li = document.createElement('li');
+    li.textContent = 'something new to do';
+    //ul.append(li);
+    ul.prepend(li);
 })
 
 const items = document.querySelectorAll('li');
 
 items.forEach(item => { 
     item.addEventListener('click', e => {
-        console.log('you clicked the item');
-        console.log(e);
-        console.log(e.target);
-        e.target.style.textDecoration = 'line-through';
+        e.target.remove();
     })
 })
+
