6ecddc1ed53dfa0908a951b80b6de4aebee5abf8
= <=>
2022-06-26 17:19:45 -0400

    Testing RegEx Patterns

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,5 @@
-// Submit Events
-
+// Testing RexEx Patterns
 const form = document.querySelector('.signup-form');
-const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
     e.preventDefault(); // submit action refershed page by default unless you have this
@@ -9,3 +7,24 @@ form.addEventListener('submit', e => {
     console.log(form.username.value); // another way to get a field from a form
 
 });
+
+
+// testing RegEx
+const username = 'shaunp';
+const pattern = /^[a-z]{6,}$/
+
+// let result = pattern.test(username);
+
+// if(result) {
+//     console.log('regex test passed :)');
+// } else {
+//     console.log('regex test failed :(');
+// }
+
+
+// returns the index of where the start of the pattern is found
+// const username = '123shaunp55';
+// const pattern = /[a-z]{6,}/
+let result = username.search(pattern);
+console.log(result);
+
