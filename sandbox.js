2491c7064192cdfc4fc3d36ca2153b930b335960
Brian Harford <baharford@gmail.com>
2022-06-26 13:50:36 -0400

    Adding & Changing Page Content

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,30 @@
-// get an element by ID
-const title = document.getElementById('page-title');
-console.log(title);
+// adding and changing page content
 
-// get elements by their class name
-const errors = document.getElementsByClassName('error');
-console.log(errors);
-console.log(errors[0]);
+const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome';
+
+const paras = document.querySelectorAll('p');
+
+// change the text of something
+// paras.forEach(para => { 
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// });
 
 
-// get elements by their tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[0]);
+// change the html of something
+const content = document.querySelector('.content');
+
+// //console.log(content.innerHTML);
+// content.innerHTML += '<h2>THIS IS A NEW H2</h2>';
+
+
+const people = ['mario', 'luigi', 'yoshi']; 
+
+// cycle through the people and generate some html template
+people.forEach(person => { 
+    content.innerHTML += `<p>${person}</p>`;
+});
 
