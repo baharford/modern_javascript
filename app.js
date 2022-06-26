c1760378f84b088e9ab207b15eba347d67ba09a4
= <=>
2022-06-26 19:33:43 -0400

    Intervals & Animating the Score

--- app.js
+++ app.js
@@ -21,8 +21,17 @@ form.addEventListener('submit', e => {
 
     // show result on page
     scrollTo(0,0);
-    result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score) { 
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
 
     // window object (global object)
 
@@ -38,5 +47,19 @@ form.addEventListener('submit', e => {
     // setTimeout(() => {
     //     alert('hello, ninjas');
     // }, 3000);
+
+
+
+    // intervals 
+
+    // let i = 0;
+
+    // const timer = setInterval(() => {
+    //     console.log('hello');
+    //     i++;
+    //     if(i === 5) { 
+    //         clearInterval(timer);
+    //     }
+    // }, 1000);
 
 });
