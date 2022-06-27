11e6d49004bc26bd004cc587c2745585e6aae1a1
Brian Harford <baharford@gmail.com>
2022-06-27 13:53:59 -0400

    JSON Data

--- sandbox.js
+++ sandbox.js
@@ -11,7 +11,8 @@ const getTodos = (callback) => {
         }
     });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    request.open('GET', 'todos.json');
     request.send();
 }
 
