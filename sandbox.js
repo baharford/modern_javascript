30388f8ccad6672851a3e171d7d3a759e8172aee
Brian Harford <baharford@gmail.com>
2022-06-27 14:47:38 -0400

    The Fetch API

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,19 @@
-// async code in action
-const getTodos = (resource) => { 
-    return new Promise((resolve, reject) => { 
-        const request = new XMLHttpRequest();
+// fetch api
 
-        request.addEventListener('readystatechange', () => {
-            //console.log(request, request.readyState);
-            if(request.readyState === 4 && request.status === 200) { 
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if (request.readyState === 4) {
-                reject('error getting resource');
-            }
-        });
-    
-        request.open('GET', resource);
-        request.send();
-    });
-}
-
-// promise chaining (no callback hell)
-getTodos('todos/luigi.json').then(data => {
-    console.log('promise resolved: ', data);
-    return getTodos('todos/mario.json'); // returns a promise
-}).then(data => { // can use this to tack on new promises
-    console.log('promise 2 resolved', data);
-    return getTodos('todos/shaun.json'); // returns a promise
-}).then(data => { 
-    console.log('promise 3 resolved', data);
-}).catch(err => {
-    console.log('promise rejected:', err);
+fetch('todos/luigi.json').then((response) => {
+    if(response.status === 200) { 
+        console.log('resolved', response);
+        return response.json(); // returns a promise
+    } else { 
+        console.log('error: ' + response.status);
+    }
+}).then(data => {
+    console.log(data);
+}).catch((err) => {
+    console.log('rejected', err); // promise only ever rejected if you get some kind of network error
 });
+
+
+// first fetch the data
+// then return response.json
+// then use the data
