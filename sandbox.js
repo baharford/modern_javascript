343312fb5c773dd2d7b04d59ba655bd2af2d30b7
Brian Harford <baharford@gmail.com>
2022-06-28 17:05:01 -0400

    Find Method

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,8 @@
-// reduce method -- returns a value based on an array
-//const scores = [10, 20, 60, 40, 70, 90, 30];
+// find method -- loops through until it finds condition and then stops
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-// simple example -- get a count of scores > 50
-// const result = scores.reduce((acc, curr) => { // acc is like a running total, curr is current value
-//     if(curr > 50) {
-//         acc++;
-//     }
-//     return acc;
-// }, 0); // this argument is the initial value of acc
+const firstHighScore = scores.find(score => { 
+    return score > 50;
+})
 
-// console.log(result);
-
-
-// a more complex example -- use reduce to add up total score of mario
-const scores = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60}
-]
-
-const marioTotal = scores.reduce((acc, curr) => {
-    if(curr.player === 'mario') {
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
-
-console.log(marioTotal);
+console.log(firstHighScore);
