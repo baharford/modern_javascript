1526b9740f7a6f7f14c37f12291fde0de385c7c5
Brian Harford <baharford@gmail.com>
2022-06-29 08:03:14 -0400

    Chaining Array Methods

--- sandbox.js
+++ sandbox.js
@@ -1,44 +1,30 @@
-// sort method - sorting strings
-const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
+// chain array methods
 
-//names.sort(); // automatically sorts names alphabetically, directly changes the array (destructive method)
-//names.reverse();
-names.sort().reverse(); // can chain them together to get rever alphabetial order
-console.log(names);
+const products = [
+    {name: 'gold star', price: 30},
+    {name: 'green shell', price: 10},
+    {name: 'red shell', price: 40},
+    {name: 'banana skin', price: 5},
+    {name: 'mushroom', price: 50},
+];
 
-// example 2 - sorting numbers
-const scores = [10, 50, 20, 5, 35, 70, 45];
-//scores.sort(); // <- this only sorts by first number, so 5 appears greater than 20
-scores.sort((a, b) => {
-    return b - a;
-});
-console.log(scores);
+// traditional way
 
-// example 3 - sorting objects
-const players = [
-    {name: 'mario', score: 20},
-    {name: 'luigi', score: 10},
-    {name: 'chun-li', score: 50},
-    {name: 'yoshi', score: 30},
-    {name: 'shaun', score: 70},
-]
+// only keep items in the array with price > 30 , and then display some text of them with a sale price
+// const filtered = products.filter(product => product.price > 20);
 
-// sort by score descending (complex way)
-// players.sort((a,b) => { 
-//     if(a.score > b.score) { // is score of 1st one greater than score of 2nd one?
-//         return -1; // if true, then -1 means a will come first
-//     } else if (b.score > a.score) {
-//         return 1; // if false, then swap them (by returning the -1)
-//     } else {
-//         return 0; // if no change is needed
-//     }
+// const promos = filtered.map(product => {
+//     return `the ${product.name} is ${product.price / 2} pounds`
 // });
 
-// console.log(players);
+// console.log(promos);
 
-// simpler way
-players.sort((a, b) => { 
-    return b.score - a.score;
-});
 
-console.log(players);
+
+
+// chaining methods
+const promos = products
+    .filter(product => product.price > 20)
+    .map(product => `the ${product.name} is ${product.price / 2} pounds`);
+
+console.log(promos);
