8e8666ad7616b705605834dcb045d903ef49a3c0
Brian Harford <baharford@gmail.com>
2022-06-30 17:01:03 -0400

    Class Constructors

--- js_chapter15/sandbox.js
+++ js_chapter15/sandbox.js
@@ -1,33 +1,20 @@
-// lots of duplication this way (object literals)... :(
-const userOne = { 
-    username: 'ryu',
-    email: 'ryu@thenetninja.co.uk',
-    login() { 
-        console.log('the user logged in');
-    },
-    logout() { 
-        console.log('the user logged out');
+// classes
+class User { 
+    constructor(username, email){ 
+        // set up properties
+        this.username = username;
+        this.email = email;
     }
 }
 
-const userTwo = { 
-    username: 'chun-li',
-    email: 'chun-li@thenetninja.co.uk',
-    login() { 
-        console.log('the user logged in');
-    },
-    logout() { 
-        console.log('the user logged out');
-    }
-}
+// the 'new' keyword
+// 1 - it creates a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor function to 'build' the object
 
-console.log(userOne.email, userOne.email);
-userOne.login();
+const userOne = new User('mario', 'mario@thenetninja.co.uk'); // an 'instance' of the object
+const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
 
-console.log(userTwo.email, userTwo.email);
-userTwo.login();
-
-
-// would be nice to be able to do something like this ... (in next chapter)
-const userThree = new User('shaun@thenetninja.co.uk', 'shaun');
+console.log('userOne', userOne);
+console.log('userTwo', userTwo);
 
