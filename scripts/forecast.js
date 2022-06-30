12fcbbcc850a9daeaadb2ecb231dae73de6cb291
Brian Harford <baharford@gmail.com>
2022-06-30 10:17:47 -0400

    Get Weather API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,21 @@
 const key = 'ZF0zTjOsYTQy9SMp1AQXaskzDuFQPrjN' // api key for accuweather api
 //const city = document.querySelector('')
 
+
+// get weather info
+const getWeather = async (id) => { 
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// get city info
 const getCity = async (city) => { 
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,8 +27,13 @@ const getCity = async (city) => {
     //console.log(data[0]); // first element is closest match
     return data[0];
 
-}
+};
 
-// getCity('manchester')
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+// getCity('manchester').then(data => { 
+//     return getWeather(data.Key);
+// }).then(data => {
+//     console.log(data);
+// }).catch(err => console.log(err));
+
+
+// do something with this weather info
