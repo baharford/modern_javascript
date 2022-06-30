a614aaa5f858490631299f6235929ac20044ed5d
Brian Harford <baharford@gmail.com>
2022-06-30 11:10:03 -0400

    Updating the UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,29 @@
 // for dom manipulation
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => { 
+
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+    `;
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){ 
+        card.classList.remove('d-none');
+    }
+
+}
 
 const updateCity = async (city) => {
 
@@ -22,8 +46,8 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     // update the ui with the new city
-    // updateCity(city)
-    // .then(data => console.log(data))
-    // .catch(err => console.log(err));
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 
 });
