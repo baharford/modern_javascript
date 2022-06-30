2ab50bbf317a3d1b5ea14cc90323710fa029b31c
Brian Harford <baharford@gmail.com>
2022-06-30 13:10:11 -0400

    Stringify & Parsing Data

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,15 @@
-// store data in local storage
-localStorage.setItem('name', 'mario');
-localStorage.setItem('age', 50);
+const todos = [
+    {text: 'play mariokart', author: 'shaun'},
+    {text: 'buy some milk', author: 'mario'},
+    {text: 'buy some bread', author: 'luigi'},
+];
 
-// get data from local storage
-let name = localStorage.getItem('name');
-let age = localStorage.getItem('age');
+// console.log(todos);
+// console.log(JSON.stringify(todos));
 
-console.log('original name and age', name, age);
+localStorage.setItem('todos', JSON.stringify(todos));
 
-// updating data
-localStorage.setItem('name', 'luigi');
-localStorage.age = '40';
-
-name = localStorage.getItem('name');
-age = localStorage.getItem('age');
-console.log('updated name and age', name, age);
-
-// deleting data from local storage
-// localStorage.removeItem('name');
-localStorage.clear(); // empties everything
-
-name = localStorage.getItem('name');
-age = localStorage.getItem('age');
-
-console.log(name, age);
+const stored = localStorage.getItem('todos');
+console.log(stored);
+console.log(JSON.parse(stored));
 
