028f60f13ce0b9a4864f74feb18f1dc0616eb4b9
Brian Harford <baharford@gmail.com>
2022-06-30 10:02:31 -0400

    Get City API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,2 +1,19 @@
 const key = 'ZF0zTjOsYTQy9SMp1AQXaskzDuFQPrjN' // api key for accuweather api
+//const city = document.querySelector('')
 
+const getCity = async (city) => { 
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    //console.log(data[0]); // first element is closest match
+    return data[0];
+
+}
+
+// getCity('manchester')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
