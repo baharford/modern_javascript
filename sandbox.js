8c85248b411c4605bd2bf3fd5241479436a1050d
Brian Harford <baharford@gmail.com>
2022-06-30 08:50:53 -0400

    Date-fns Library

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,21 @@
-// building a digital clock
-const clock = document.querySelector('.clock');
+// date-fns
 
-const tick = () => { 
+const now = new Date();
+// const now = new Date(234523453);
+// console.log(dateFns.isToday(now));
 
-    const now = new Date();
 
-    const h = now.getHours();
-    const m = now.getMinutes();
-    const s = now.getSeconds();
+// formatting options
+console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, 'MMMM'));
+console.log(dateFns.format(now, 'MMM'));
+console.log(dateFns.format(now, 'dddd'));
+console.log(dateFns.format(now, 'Do'));
+console.log(dateFns.format(now, 'dddd Do MMMM, YYYY'));
 
-    const html = `
-        <span>${h}</span> : 
-        <span>${m}</span> : 
-        <span>${s}</span>
-    `
-    //console.log(h, m, s);
-    clock.innerHTML = html;
 
-};
+// comparing dates
+const before = new Date('February 1 2019 12:00:00');
 
-setInterval(tick, 1000);
+console.log(dateFns.distanceInWords(now, before));
+console.log(dateFns.distanceInWords(now, before, {addSuffix: true}));
