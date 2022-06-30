e60f72e202771adce4fbf808f0d7755678fa9cf0
Brian Harford <baharford@gmail.com>
2022-06-30 10:51:03 -0400

    Updating the Location

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,5 @@
+// for interacting with apis
 const key = 'ZF0zTjOsYTQy9SMp1AQXaskzDuFQPrjN' // api key for accuweather api
-//const city = document.querySelector('')
-
 
 // get weather info
 const getWeather = async (id) => { 
