39fdd579e4a444ebc1aa261c51aaee788f4ca8cd
Brian Harford <baharford@gmail.com>
2022-06-30 17:53:35 -0400

    Constructors (under the hood)

--- js_chapter15/sandbox.js
+++ js_chapter15/sandbox.js
@@ -1,60 +1,29 @@
-// classes
-class User { 
-    constructor(username, email){ 
-        // set up properties
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-    login() { 
-        console.log(`${this.username} just logged in`);
-        return this;
-    }
-    logout() { 
-        console.log(`${this.username} just logged out`);
-        return this;
-    }
-    incScore() { 
-        this.score += 1;
-        console.log(`${this.username} has a score of ${this.score}`);
-        return this;
+// constructor functions
+
+// this is the same thing as the class below
+function User(username, email){ 
+    this.username = username;
+    this.email = email;
+    this.login = function() {
+        console.log(`${this.username} has logged in.`);
     }
 }
 
-class Admin extends User { 
-    constructor(username, email, title) {
-        super(username, email);
-        this.title = title;
-    }
-    deleteUser(userToDelete) { 
-        users = users.filter(currentUser => currentUser.username !== userToDelete.username);
-    }
-}
+// this is the new way to create objects (syntatical sugar)
+// class User { 
+//     constructor(username, email){ 
+//         // set up properties
+//         this.username = username;
+//         this.email = email;
+//     }
+// }
+
+const userOne = new User('mario', 'mario@thenetninja.co.uk'); // an 'instance' of the object
+const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+userOne.login();
 
 // the 'new' keyword
 // 1 - it creates a new empty object {}
 // 2 - it binds the value of 'this' to the new empty object
 // 3 - it calls the constructor function to 'build' the object
-
-const userOne = new User('mario', 'mario@thenetninja.co.uk'); // an 'instance' of the object
-const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
-const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk', 'black-belt-ninja');
-
-console.log('userOne', userOne);
-console.log('userTwo', userTwo);
-
-// userOne.login();
-// userTwo.login();
-
-// userOne.logout();
-// userTwo.logout();
-
-userOne.login().incScore().incScore().incScore().logout(); // method chaining possible when you return this
-console.log(userThree);
-
-let users = [userOne, userTwo, userThree];
-console.log(users);
-
-userThree.deleteUser(userTwo);
-console.log(users);
 
