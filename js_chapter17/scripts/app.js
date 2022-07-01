3988d8689d643e52fb67853e436aaed4d7a6b1b3
Brian Harford <baharford@gmail.com>
2022-07-01 19:32:31 -0400

    Updating the Room

--- js_chapter17/scripts/app.js
+++ js_chapter17/scripts/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMessage = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms')
 
 // add a new chat
 newChatForm.addEventListener('submit', e => { 
@@ -26,6 +27,16 @@ newNameForm.addEventListener('submit', e => {
     // show , then hide , the update message
     updateMessage.innerText = `Your name was updated to ${newName}!`;
     setTimeout(() => updateMessage.innerText = '', 3000);
+});
+
+// update the chat room
+rooms.addEventListener('click', e => { 
+
+    if(e.target.tagName === 'BUTTON') { 
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
 });
 
 // check localStorage for a name
