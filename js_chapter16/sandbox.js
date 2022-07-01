edfa3619f1cc5b038c74b58aa20046eeb418b29f
Brian Harford <baharford@gmail.com>
2022-07-01 15:46:00 -0400

    Deleting Documents

--- js_chapter16/sandbox.js
+++ js_chapter16/sandbox.js
@@ -1,12 +1,13 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 
-const addRecipe = (recipe) => { 
+const addRecipe = (recipe, id) => { 
     let time = recipe.created_at.toDate();
     let html = ` 
-        <li>
+        <li data-id="${id}">
             <div>${recipe.title}</div>
             <div>${time}</div>
+            <button class="btn btn-danger btn-sm my-2">delete</button>
         </li>
     `
     list.innerHTML += html; // append to list element
@@ -19,7 +20,7 @@ db.collection('recipes').get().then((snapshot) => {
     // console.log(snapshot.docs[0].data());
     snapshot.docs.forEach(doc => { 
         console.log(doc.data());
-        addRecipe(doc.data());
+        addRecipe(doc.data(), doc.id);
     });
 }).catch(err => { 
     console.log(err);
@@ -41,4 +42,14 @@ form.addEventListener('submit', e => {
         console.log(err);
     })
 
+});
+
+// deleting data 
+list.addEventListener('click', e => { 
+    if(e.target.tagName === 'BUTTON') { 
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete().then(() => { 
+            console.log('recipe deleted!');
+        });
+    }
 });
