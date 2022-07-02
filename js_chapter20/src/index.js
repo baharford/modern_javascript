251e4080da8a2b347edc41b8325b9214494eb157
Brian Harford <baharford@gmail.com>
2022-07-02 15:46:45 -0400

    Dropdowns

--- js_chapter20/src/index.js
+++ js_chapter20/src/index.js
@@ -1,12 +1,16 @@
 //import './test.css';
-import Tooltip from './ninja-ui/tooltip'
+import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 
 console.log('test');
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
-
 tooltip.init();
 
-
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => { 
+    const instance = new Dropdown(dropdown);
+    instance.init();
+});
