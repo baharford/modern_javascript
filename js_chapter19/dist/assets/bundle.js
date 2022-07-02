41708b5ecf8f8cf349128243f031e92c49649977
Brian Harford <baharford@gmail.com>
2022-07-02 11:26:58 -0400

    Webpack CLI

--- js_chapter19/dist/assets/bundle.js
+++ js_chapter19/dist/assets/bundle.js
@@ -1,9 +1,1 @@
-"use strict";
-
-var greet = function greet(name) {
-  console.log("hello ".concat(name));
-};
-
-greet('mario');
-greet('luigi');
-greet('link');
+(()=>{const l=l=>{console.log(`hello ${l}`)};l("mario"),l("luigi"),l("link")})();
