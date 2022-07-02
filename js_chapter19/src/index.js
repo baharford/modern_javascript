5d20a3f0a04e92e85bab651391ff84f5cdfd65fc
Brian Harford <baharford@gmail.com>
2022-07-02 11:45:00 -0400

    Default Exports

--- js_chapter19/src/index.js
+++ js_chapter19/src/index.js
@@ -1,5 +1,11 @@
 import {styleBody, addTitle, contact} from  './dom';
+import users, {getPremUsers} from './data';
 
 console.log('index file');
 addTitle('new test title');
 console.log(contact);
+
+console.log('all users: ', users);
+console.log('premium users: ', getPremUsers(users));
+
+
