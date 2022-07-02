d8fcb7bfb7d4b7e89a9eb0ab8ed67e2c57e75a70
Brian Harford <baharford@gmail.com>
2022-07-02 10:00:33 -0400

    NPM Scripts & Watching Files

---
+++ js_chapter19/before.js
@@ -0,0 +1,6 @@
+// ran command to use babel cli to transform this ito old code: node_modules/.bin/babel before.js -o after.js
+const greet = (name) => { 
+    console.log(`hello ${name}`);
+};
+
+greet();
