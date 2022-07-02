88502f15dda8b7997e4e92ee7e88ede4181800c5
Brian Harford <baharford@gmail.com>
2022-07-02 13:38:05 -0400

    Tooltips

--- js_chapter20/src/index.js
+++ js_chapter20/src/index.js
@@ -1,3 +1,12 @@
-import './test.css';
+//import './test.css';
+import Tooltip from './ninja-ui/tooltip'
 
 console.log('test');
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+
+tooltip.init();
+
+
