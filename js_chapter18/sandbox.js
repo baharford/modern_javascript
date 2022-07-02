f0cb20819caa42163b0c47dc3e7877df87bf3092
Brian Harford <baharford@gmail.com>
2022-07-01 20:01:15 -0400

    Symbols

--- js_chapter18/sandbox.js
+++ js_chapter18/sandbox.js
@@ -1,26 +1,21 @@
-// sets -- like arrays but with unique values 
-const namesArray = ['ryu', 'chun-li', 'ryu', 'shaun'];
-console.log(namesArray);
+// symbols - can be used as keys or property names in objects
 
-// const namesSet = new Set(['ryu', 'chun-li', 'ryu', 'shaun']);  // <- does same thing as below
-// const namesSet = new Set(namesArray);
-// console.log(namesSet);
+const symbolOne = Symbol();
+const symbolTwo = Symbol();
 
-// const uniqueNames = [...namesSet];
-const uniqueNames = [...new Set(namesArray)];
-console.log(uniqueNames);
+console.log(symbolOne, symbolTwo, typeof symbolOne);
+
+console.log(symbolOne === symbolTwo); // false , symbols are always unique
 
 
-// sets also have their own unique set of methods (different than arrays)
-const ages = new Set();
-ages.add(20);
-ages.add(25);
-console.log(ages);
-ages.delete(25, ages.size); // sets use *.size rather than *.length to get size
+const ninja = {};
+ninja.age = 30;
+ninja['belt'] = 'orange';
+ninja['belt'] = 'black';
 
-console.log(ages.has(30)); // returns boolean
+ninja[symbolOne] = 'ryu';
+ninja[symbolTwo] = 'shaun';
 
-ages.clear();
-console.log(ages);
+console.log(ninja);
 
 
