50f8e1c7c25f80ad6ecce23b3a39cc385b4f98fb
Brian Harford <baharford@gmail.com>
2022-07-02 12:32:16 -0400

    Babel & Webpack Together

--- js_chapter19/webpack.config.js
+++ js_chapter19/webpack.config.js
@@ -14,6 +14,19 @@ module.exports = {
             directory: path.resolve(__dirname, 'dist')
         },
         compress: true
+    },
+    // allows you to run babel to convert to older version of js prior to building with webpack and minifying
+    module: { 
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: { 
+                loader: 'babel-loader',
+                options: { 
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
     }
 };
 
