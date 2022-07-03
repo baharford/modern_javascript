d686854a7738ea97725496a1553b50a37b9770f3
Brian Harford <baharford@gmail.com>
2022-07-03 10:00:05 -0400

    Creating a Firebase Project

--- js_chapter21/src/index.js
+++ js_chapter21/src/index.js
@@ -1,1 +1,12 @@
-console.log('hello from index.js');
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDPF3p2XpHxqhzrtPoAFdR9jboD93P_FPY",
+    authDomain: "fir-9-dojo-b968b.firebaseapp.com",
+    projectId: "fir-9-dojo-b968b",
+    storageBucket: "fir-9-dojo-b968b.appspot.com",
+    messagingSenderId: "1091363061896",
+    appId: "1:1091363061896:web:bbec033e0d09593b6a5f3d"
+  };
+
+initializeApp(firebaseConfig);
