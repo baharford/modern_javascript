2b613e52914da9d18400c295b2307ec4e58d6d12
Brian Harford <baharford@gmail.com>
2022-07-03 09:38:29 -0400

    Snackbar

--- js_chapter20/src/index.js
+++ js_chapter20/src/index.js
@@ -2,6 +2,7 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
 import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 console.log('test');
 
@@ -19,3 +20,12 @@ dropdowns.forEach(dropdown => {
 // create tabs
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+// create snackbar 
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', e => { 
+    snackbar.show('you clicked me :)');
+});
